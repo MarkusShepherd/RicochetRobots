7acd33f3ce018c5e5e64dd26f2c57507966dc6c7
Markus Schepke <markus.schepke@censhare.com>
2014-03-24 09:22:08 +0000

    work on Solver.java

--- src/info/riemannhypothesis/ricochetrobots/Solver.java
+++ src/info/riemannhypothesis/ricochetrobots/Solver.java
@@ -3,26 +3,37 @@
  */
 package info.riemannhypothesis.ricochetrobots;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author MarkusSchepke
  * 
  */
 public class Solver {
-    private final List<Robot> robots;
+    // private final Set<Robot> robots;
+    private final Map<Robot, Integer> indexes;
     private final Board board;
     private final Point target;
     private final Robot targetRobot;
 
-    private final Iterable<List<Point>> solution;
+    private final Iterable<Point[]> solution;
 
-    public Solver(Board board, List<Robot> robots, Point target,
+    public Solver(Board board, Set<Robot> robots, Point target,
             Robot targetRobot) {
+        assert robots.contains(targetRobot);
         this.board = board;
-        this.robots = robots;
+        // this.robots = robots;
         this.target = target;
         this.targetRobot = targetRobot;
+
+        int counter = 0;
+        indexes = new HashMap<Robot, Integer>();
+        for (Robot robot : robots) {
+            indexes.put(robot, counter++);
+        }
 
         this.solution = brute();
     }
@@ -30,10 +41,20 @@ public class Solver {
     /**
      * @return
      */
-    private Iterable<List<Point>> brute() {
-        // TODO Auto-generated method stub
-        return null;
+    private Iterable<Point[]> brute() {
+        LinkedList<Point[]> result = new LinkedList<Point[]>();
+        return result;
     }
 
-    
+    private class Node {
+        private final Point[] configuration;
+        private final int moves;
+        private final Node previous;
+
+        private Node(Point[] configuration, int moves, Node previous) {
+            this.configuration = configuration;
+            this.moves = moves;
+            this.previous = previous;
+        }
+    }
 }
