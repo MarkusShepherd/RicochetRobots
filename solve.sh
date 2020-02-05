3410be20cb6be925418ab3788427158ab21bbe88
Markus Shepherd <markus.schepke@maas.global>
2020-02-05 15:59:38 +0200

    move Java code into java dir

--- solve.sh
+++ solve.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-java -ea -cp bin:lib/commons-cli-1.3.1.jar info.riemannhypothesis.ricochetrobots.Solver $@
+java -ea \
+	-classpath bin:java/lib/commons-cli-1.3.1.jar \
+    info.riemannhypothesis.ricochetrobots.Solver "$@"
